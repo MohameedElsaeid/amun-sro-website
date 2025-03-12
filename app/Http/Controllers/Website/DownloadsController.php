1f3ad5ae296419e4948b1e1bd444288a6a944d06
Mohamed Elsaed <m.ashraf@converted.in>
2025-03-12 22:02:03 +0200

    add Exception

--- app/Http/Controllers/Website/DownloadsController.php
+++ app/Http/Controllers/Website/DownloadsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use Exception;
+use Log;
 use Storage;
 
 class DownloadsController extends Controller
@@ -15,7 +17,12 @@ class DownloadsController extends Controller
 
     public function sbot()
     {
-        $downloadUrl = Storage::disk('s3')->temporaryUrl('downloads/AmunSroSBotP.rar', Carbon::now()->addMinutes(5));
-        return redirect($downloadUrl);
+        try {
+            $downloadUrl = Storage::disk('s3')->temporaryUrl('downloads/AmunSroSBotP.rar', Carbon::now()->addMinutes(5));
+            return redirect($downloadUrl);
+        } catch (Exception $e) {
+            Log::error('Download failed: ' . $e->getMessage());
+            abort(500, 'Download error. Please try again later.');
+        }
     }
 }
