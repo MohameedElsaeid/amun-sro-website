c392f5520094c29e3807f73182f5adc315cff467
Mohamed Elsaed <m.ashraf@converted.in>
2025-03-14 20:59:15 +0200

    add email job

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
@@ -53,13 +54,13 @@ return [
             'transport' => 'ses',
         ],
 
-        'postmark' => [
+        'postmark' => array(
             'transport' => 'postmark',
             // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
             // 'client' => [
             //     'timeout' => 5,
             // ],
-        ],
+        ),
 
         'resend' => [
             'transport' => 'resend',
