a376d59c3d2c8478be2bbc2b09f33b4668b87163
Mohamed Elsaed <m.ashraf@converted.in>
2025-03-14 08:51:41 +0200

    regit event

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Mail\UserRegisterEmail;
+use App\Jobs\CompleteRegistrationJob;
+use App\Jobs\CompleteRejestraiobJob;
 use App\Models\User;
 use App\Services\Facebook\ConversionEventService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Mail;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 
 class RegisterController extends Controller
 {
@@ -58,17 +55,14 @@ class RegisterController extends Controller
      * Handle a registration request for the application.
      *
      * @param Request $request
-     * @param ConversionEventService $conversionService
      * @return RedirectResponse|JsonResponse
-     * @throws ConnectionException
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
      */
-    public function register(Request $request, ConversionEventService $conversionService)
+    public function register(Request $request)
     {
         $this->validator($request->all())->validate();
 
         $registerIp = $request->ip();
+
         event(new Registered($user = $this->create($request->all(), $registerIp)));
 
         $this->guard()->login($user);
@@ -76,16 +70,10 @@ class RegisterController extends Controller
             return $response;
         }
 
-        $conversionService->trackRegister(
-            userData: [
-                'em' => $user->Email,
-                'fn' => $user->StrUserID,
-            ],
-        );
-
-        Mail::to($user->Email)
-            ->queue((new UserRegisterEmail())
-                ->onQueue('emails'));
+        CompleteRegistrationJob::dispatch([
+            'em' => $user->Email,
+            'fn' => $user->StrUserID,
+        ]);
 
         return $request->wantsJson()
             ? new JsonResponse([], 201)
---
+++ app/Jobs/CompleteRegistrationJob.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Facebook\ConversionEventService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Http\Client\ConnectionException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class CompleteRegistrationJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private readonly array $userData, private readonly array $customData = [])
+    {
+    }
+
+    /**
+     * Execute the job.
+     * @param ConversionEventService $conversionService
+     * @throws ConnectionException
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function handle(ConversionEventService $conversionService): void
+    {
+        $conversionService->trackRegister(userData: $this->userData, customData: $this->customData);
+    }
+}
