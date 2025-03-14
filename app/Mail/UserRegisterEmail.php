d9fa5fecc9fd6b081c88c76f5f22cf71115aeccc
Mohamed Elsaed <m.ashraf@converted.in>
2025-03-14 20:29:29 +0200

    add email job

--- app/Mail/UserRegisterEmail.php
+++ app/Mail/UserRegisterEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class UserRegisterEmail extends Mailable implements ShouldQueue
+class UserRegisterEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
